02efe41883f699e1a0c9100b39a23075a7846b8c
Carolyn Dorren <cdorren@er-d6.kzn.21-school.ru>
2021-02-17 10:46:39 +0300

    upButton added

--- script/main.js
+++ script/main.js
@@ -1,9 +1,12 @@
-const links = document.querySelectorAll('a[href*="#"]');
-const projects = document.querySelectorAll('.project');
+"use strict"
+
+const links = document.querySelectorAll('a[href*="#"]'),
+      projects = document.querySelectorAll('.project'),
+      upButton = document.querySelector('.up');
 
 for (let link of links) {
   link.addEventListener('click', (e) => {
-    e.preventDefault();
+    e.preventDefault()
     const link_id = link.getAttribute('href').substr(1);
 
     document.getElementById(link_id).scrollIntoView({
@@ -15,14 +18,39 @@ for (let link of links) {
 
 for (let project of projects) {
   project.addEventListener('mouseover', () => {
-    let title = project.querySelector('.pr_title');
-    title.classList.remove('hide');
+    let title = project.querySelector('.pr_title')
+    title.classList.remove('hide')
   })
 }
 
 for (let project of projects) {
   project.addEventListener('mouseout', () => {
-    let title = project.querySelector('.pr_title');
-    title.classList.add('hide');
+    let title = project.querySelector('.pr_title')
+    title.classList.add('hide')
   })
 }
+
+const buttonToggle = (bool) => {
+  if (bool) {
+    upButton.classList.remove('hide');
+  }
+  else {
+    upButton.classList.add('hide');
+  }
+}
+
+const trackScroll = () => {
+  const scrolled = window.pageYOffset;
+  let coords = document.documentElement.clientHeight;
+  scrolled > coords ? buttonToggle(1) : buttonToggle(0);
+}
+
+const scrollTop = () => {
+  if (window.pageYOffset > 0) {
+    window.scrollBy(0, -30);
+    setTimeout(scrollTop, 0);
+  }
+}
+
+window.addEventListener('scroll', trackScroll)
+upButton.addEventListener('click', scrollTop);
